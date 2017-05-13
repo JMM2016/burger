cabe76d3cdfc690a69ee5664e70b23181bc92e8f
jmm2016 <sus.magallanes@gmail.com>
2017-05-13 10:08:40 -0500

    added models

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,18 +6,40 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get("/", function(req, res) {
-  res.render("index")
+router.get("/", function(req, res) { // can be anon fun?
+  // res.render("index")
 
-  // burger.all( data => {
-  //   let hbsObj = {
-  //     burgers: data
-  //   }
-  //   console.log(hbsObj)
-  //   res.render("index", hbsObj)
-  // })
+  burger.readAll( data => {
+     let hbsObj = {
+       burgers: data
+     }
+     console.log(hbsObj)
+     res.render("index", hbsObj)
+  })
+}
+
+router.post("/", function(req, res) {			
+	burger.create([
+		"burger_name", "devoured"
+	], [
+		param.body.name, param.body.devoured
+	], () => {
+     console.log("post: " + param.body)
+     res.render("index") // Or: res.redirect("/")
+  })	
 })
 
+router.put("/:id", function(req, res) {
+	const condition = "id = " + req.params.id	
+	
+	burger.update({
+		burger_name: req.params.name,
+		devoured: req.params.devoured		// devoured sh/would also equal true here
+		} , condition, () => {
+     console.log("put: " + param.body)
+     res.render("index") // Or: res.redirect("/")
+  })
+}
 
 module.exports = router
 
---
+++ models/burger.js
@@ -0,0 +1,23 @@
+const orm = require ("../config/orm.js")
+
+const burger = {
+	readAll: function (cb){ // can these be anonymous funs??
+		orm.selectAll("burgers", res => {
+			cb(res)
+		}	
+	}
+	
+	create: function (cols, vals, cb){ // can these be anonymous funs??
+		orm.insertOne("burgers", cols, vals, res => {
+			cb(res)
+		}	
+	}
+
+	update: function (colVals, condition, cb){ // can these be anonymous funs??
+		orm.updateOne("burgers", colVals, condition, res => {
+			cb(res)
+		}	
+	}	
+}
+
+module.exports = burger
